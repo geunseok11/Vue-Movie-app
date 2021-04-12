83e28b6e407380602385d7a87d488e6a76b6ebc5
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-04-12 22:21:00 +0900

    SETTING vue-movie-app

---
+++ vue-movie-app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-container>
+      <search-bar />
+      <movie-list />
+      <!-- 검색바 -->
+      <!-- 영화 목록 -->
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import SearchBar from '@/components/SearchBar'
+import MovieList from '@/components/MovieList'
+export default {
+  components:{
+    SearchBar,
+    MovieList
+  }
+};
+</script>
